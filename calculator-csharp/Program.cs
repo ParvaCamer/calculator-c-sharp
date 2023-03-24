a04e5deb5afa84247dc120603d91b8b2d982f39e
Romain Camerlynck <romain.camerlynck@labom2iformation.fr>
2023-03-24 16:45:14 +0100

    Ajoutez des fichiers projet.

---
+++ calculator-csharp/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("--- Calculator ---");
+Console.WriteLine("Write your operation : you can use this operator => + | - | * | / | e | %");
+Console.WriteLine("-> Press E to exit <-");
+Console.WriteLine("Enter your first operand :");
+int firstOperand = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your operator:");
+string sign = Console.ReadLine();
+
+while (sign != "+" && sign != "-" && sign != "*" && sign != "/" && sign != "e" && sign != "%")
+{
+    Console.WriteLine("Invalid operator entered. Please enter a valid operator:");
+    sign = Console.ReadLine();
+}
+
+Console.WriteLine(" Enter your second operand :");
+int secondOperand = Convert.ToInt32(Console.ReadLine());
+
+switch (sign) {
+    case "+":
+        int add = firstOperand + secondOperand;
+        Console.WriteLine(add);
+        break;
+    case "-":
+        int subtract= firstOperand - secondOperand;
+        Console.WriteLine(subtract);
+        break;
+    case "*":
+        int times = firstOperand * secondOperand;
+        Console.WriteLine(times);
+        break;
+    case "/":
+        int divide = firstOperand / secondOperand;
+        Console.WriteLine(divide);
+        break;
+    case "e":
+        double power = Math.Pow(firstOperand, secondOperand);
+        Console.WriteLine(power);
+        break;
+    case "%":
+        int modulo = firstOperand % secondOperand;
+        Console.WriteLine(modulo);
+        break;
+}
